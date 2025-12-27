c9836e0638ef9cfdab815f61de0e46438e4e9dd6
Nilzao <288571+nilzao@users.noreply.github.com>
2025-12-27 18:17:49 -0400

    openfire 5.0.2

--- openfire-download.sh
+++ openfire-download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 FILE=openfire.tar.gz
 if [ ! -f "$FILE" ]; then
-    wget https://github.com/igniterealtime/Openfire/releases/download/v5.0.3/openfire_5_0_3.tar.gz -nv -O ${FILE}
+    wget https://github.com/igniterealtime/Openfire/releases/download/v5.0.2/openfire_5_0_2.tar.gz -nv -O ${FILE}
 fi
