eb12b89ca00f17e153b2adf535a742cad7b8ea93
Nilzao <288571+nilzao@users.noreply.github.com>
2025-12-28 10:02:02 -0400

    add readiness checks

--- readiness.sh
+++ readiness.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 adminpassword=${ADMIN_PASSWORD:-"admin"}
+restApiSecret=${REST_API_SECRET:-"myrestapisecret"}
 
 runLogin () {
   header=$(curl -Is 'http://localhost:9090/login.jsp')
@@ -16,6 +17,18 @@ runLogin
 HTTPCODE=$(curl -o /dev/null -I -s -w "%{http_code}\n" "http://localhost:9090/plugins/restapi/v1/system/readiness" -b "JSESSIONID=$JSESSIONID")
 
 if [ "$HTTPCODE" = "200" ]; then
+  EXPECTED='{"key":"adminConsole.access.allow-wildcards-in-excludes","value":"true"}'
+  RESPONSE=$(curl -s \
+    'http://localhost:9090/plugins/restapi/v1/system/properties/adminConsole.access.allow-wildcards-in-excludes' \
+    -H 'accept: application/json' \
+    -H "Authorization: ${restApiSecret}")
+  echo "Expected: [${EXPECTED}]"
+  echo "Response: [${RESPONSE}]"
+  diff -q <(echo "$EXPECTED") <(echo "$RESPONSE")
+  if ! diff -q <(echo "$EXPECTED") <(echo "$RESPONSE") >/dev/null; then
+    exit 1
+  fi
+  sleep 10
   exit 0
 fi
 exit 1
